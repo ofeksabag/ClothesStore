e3873c3fd1ae9abc684e027cedf18aaacea7cb40
ofeksabag <osabag55@gmail.com>
2023-03-14 01:26:27 +0200

    .

--- Backend/src/5-services/product-service.ts
+++ Backend/src/5-services/product-service.ts
@@ -1,6 +1,11 @@
+import { OkPacket } from "mysql";
 import dal from "../2-utils/dal";
 import BrandModel from "../4-models/brand-model";
+import CartModel from "../4-models/cart-model";
+import CartItemsModel from "../4-models/cartItems-model";
 import CategoryModel from "../4-models/category-model";
+import { ResourceNotFoundError } from "../4-models/client-errors";
+import OrderModel from "../4-models/order-model";
 import ProductModel from "../4-models/product-model";
 import SubcategoryModel from "../4-models/subcategory-model";
 
@@ -28,9 +33,56 @@ async function getAllProducts(): Promise<ProductModel[]> {
     return products;
 }
 
+async function getUserCartItems(userId: number): Promise<CartItemsModel[]> {
+    const sql = "SELECT * FROM cartItems AS CI JOIN cart AS C ON CI.cartId = C.cartId WHERE C.userId = ?";
+    const cartItems = await dal.execute(sql, userId);
+    return cartItems;
+}
+
+async function addCart(cart: CartModel): Promise<CartModel> {
+    cart.validate();
+    const sql = "INSERT INTO cart values(DEFAULT, ?, ?)";
+    const result: OkPacket = await dal.execute(sql, cart.userId, cart.date);
+    cart.cartId = result.insertId;
+    return cart;
+}
+
+async function addCartItem(cartItem: CartItemsModel): Promise<CartItemsModel> {
+    cartItem.validate();
+    const sql = "INSERT INTO cartItems values(DEFAULT, ?, ?, ?)";
+    const result: OkPacket = await dal.execute(sql, cartItem.productId, cartItem.cartId, cartItem.amount);
+    cartItem.itemId = result.insertId;
+    return cartItem;
+}
+
+async function updateCartItem(cartItem: CartItemsModel): Promise<CartItemsModel> {
+    cartItem.validate();
+    const sql = "UPDATE cartItems SET productId = ?, cartId = ?, amount = ?";
+    const result: OkPacket = await dal.execute(sql, cartItem.productId, cartItem.cartId, cartItem.amount);
+    if(result.affectedRows === 0) throw new ResourceNotFoundError(cartItem.itemId);
+    return cartItem;
+}
+
+async function deleteCartItem(itemId: number): Promise<void> {
+    const sql = "DELETE FROM cartItems WHERE itemId = ?";
+    const result = await dal.execute(sql, itemId);
+    if(result.affectedRows === 0) throw new ResourceNotFoundError(itemId);
+}
+
+/*
+async function addOrder(order: OrderModel): Promise<OrderModel> {
+
+}
+*/
+
 export default {
     getAllBrands,
     getAllCategories,
     getAllSubcategories,
-    getAllProducts
+    getAllProducts,
+    getUserCartItems,
+    addCart,
+    addCartItem,
+    updateCartItem,
+    deleteCartItem
 }
--- Backend/src/6-routes/product-routes.ts
+++ Backend/src/6-routes/product-routes.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
+import CartModel from "../4-models/cart-model";
+import CartItemsModel from "../4-models/cartItems-model";
 import productService from "../5-services/product-service";
 
 const router = express.Router();
@@ -41,6 +43,67 @@ router.get("/products", async (request: Request, response: Response, next: NextF
     try {
         const products = await productService.getAllProducts();
         response.json(products);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// GET http://localhost:4000/api/cartItems/:userId
+router.get("/cartItems/:userId([0-9]+)", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const userId = +request.params.userId;
+        await productService.getUserCartItems(userId);
+        response.sendStatus(204);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// POST http://localhost:4000/api/cart
+router.post("/cart", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const cart = new CartModel(request.body);
+        const addedCart = await productService.addCart(cart);
+        response.status(201).json(addedCart);
+    }
+    catch(err: any) {
+        next(err);
+    }
+});
+
+// POST http://localhost:4000/api/cartItems
+router.post("/cartItems", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const cartItem = new CartItemsModel(request.body);
+        const addedCartItem = await productService.addCartItem(cartItem);
+        response.status(201).json(addedCartItem);
+    }
+    catch(err: any) {
+        next(err);
+    }
+});
+
+// PUT http://localhost:4000/api/cartItems/:itemId
+router.put("/cartItems/:itemId([0-9]+)", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        request.body.itemId = +request.params.itemId;
+        const cartItem = new CartItemsModel(request.body);
+        const updatedCartItem = await productService.updateCartItem(cartItem);
+        response.status(200).json(updatedCartItem);
+    }
+    catch (err: any) {
+        next(err);
+    }
+});
+
+// DELETE http://localhost:4000/api/cartItems/:itemId
+router.delete("/cartItems/:itemId([0-9]+)", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const itemId = +request.params.itemId;
+        await productService.deleteCartItem(itemId);
+        response.sendStatus(204);
     }
     catch (err: any) {
         next(err);
